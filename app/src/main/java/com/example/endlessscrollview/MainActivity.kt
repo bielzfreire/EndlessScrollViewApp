155f7993f9963ac6b10eb2d1ef4c64cb51a7ab49
Gabriel Freire <42545425+gfreiresantos@users.noreply.github.com>
2019-06-06 19:05:03 -0300

    Add material components, init retrofit services

--- app/src/main/java/com/example/endlessscrollview/MainActivity.kt
+++ app/src/main/java/com/example/endlessscrollview/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.endlessscrollview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import retrofit2.Retrofit
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var retrofit: Retrofit
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        retrofit = Retrofit.Builder()
+            .baseUrl("https://api.themoviedb.org/3/discover/movie?api_key=5da5c7cec70cc62a19883de5799a21a9&sort_by=popularity.desc&include_adult=false&include_video=false&page=1")
+            .build()
     }
 }
